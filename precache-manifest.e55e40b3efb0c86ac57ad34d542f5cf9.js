1810549395d251d555fef6882158fb964ea7cdcb
Kah Wai LIM <kwlim@ntu.edu.sg>
2019-10-28 18:28:35 +0800

    Updates

---
+++ precache-manifest.e55e40b3efb0c86ac57ad34d542f5cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2e8b909b1cc3487464dc2368d7a1ee",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "c055f6fd0e5c9a9e2844",
+    "url": "/robofriends2/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3a044172525e5de4b44d",
+    "url": "/robofriends2/static/css/main.e7b61079.chunk.css"
+  },
+  {
+    "revision": "c055f6fd0e5c9a9e2844",
+    "url": "/robofriends2/static/js/2.05fd1a14.chunk.js"
+  },
+  {
+    "revision": "3a044172525e5de4b44d",
+    "url": "/robofriends2/static/js/main.2ad510ea.chunk.js"
+  },
+  {
+    "revision": "bd045ea311be20ab51e6",
+    "url": "/robofriends2/static/js/runtime-main.8a35b2c0.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends2/static/media/SEGA.82e09f78.TTF"
+  }
+]);
